392976409ba56aaad0ed21d82a8d59cfd3c48e47
Allex Welter <allexwelter13@gmail.com>
2024-12-07 07:12:00 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
+/*  Criar nossa API de usuários
+    -Criar um usuário   
+    -Listar todos os usuários
+    -Editar um usuário
+    -Deletar um usuário
+*/
+
 import express from 'express'
 
 const app = express()
+
+const users = []
+
+app.post('/usuarios', (req, res) =>{
+    
+    console.log(req)
+
+    res.send('Ok, POST')
+
+})
 
 app.get('/usuarios', (req, res) => {
     res.send('OK, deu bom')    
