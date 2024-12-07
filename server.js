769071432e3bd0db5df4f3c8012c53093060c83b
Allex Welter <allexwelter13@gmail.com>
2024-12-07 09:29:49 -0300

    Update server.js

--- server.js
+++ server.js
@@ -13,8 +13,6 @@ const prisma = new PrismaClient()
 const app = express()
 app.use(express.json())
 
-const users = []
-
 app.post('/usuarios', async (req, res) => {
 
     await prisma.user.create({
@@ -29,7 +27,10 @@ app.post('/usuarios', async (req, res) => {
 
 })
 
-app.get('/usuarios', (req, res) => {
+app.get('/usuarios', async (req, res) => {
+    
+    const users = await prisma.user.findMany()
+    
     res.status(200).json(users)
 })
 
