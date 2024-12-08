03f28125336c239827f9cdaca9094d613059982b
Allex Welter <allexwelter13@gmail.com>
2024-12-08 08:14:24 -0300

    .

--- Front-End(Cadastro-usuarios)/cadastro-usuarios/src/pages/Home/index.jsx
+++ Front-End(Cadastro-usuarios)/cadastro-usuarios/src/pages/Home/index.jsx
@@ -3,11 +3,25 @@ import Trash from '../../assets/trash.png'
 
 function Home() {
 
-    const users = [{
-
-    }, {
-
-    }
+  const users = [
+    {
+      id: '233fdsgsg',
+      name: 'Allex',
+      address: 'rua 33',
+      email: 'allex@hotmail.com'
+    },
+    {
+      id: '244faeysg',
+      name: 'Lulu',
+      address: 'rua 44',
+      email: 'lulu@hotmail.com'
+    },
+    {
+      id: '255ftogsg',
+      name: 'Joao',
+      address: 'rua 55',
+      email: 'joao@hotmail.com'
+    },
   ]
 
   return (
@@ -15,22 +29,25 @@ function Home() {
     <div className='container'>
       <form>
         <h1>Cadastro de Usuários</h1>
-        <input name='nome' type='text'/>
-        <input name='idade' type='number'/>
-        <input name='email' type='email'/>
-        <button type='button'>Cadastrar</button>  
+        <input placeholder='Nome' name='nome' type='text' />
+        <input placeholder='Endereço' name='endereço' type='text' />
+        <input placeholder='Email' name='email' type='email' />
+        <button type='button'>Cadastrar</button>
       </form>
 
-      <div>
-        <div>
-          <p>Nome: </p>
-          <p>Idade: </p>
-          <p>Email: </p>  
+      {users.map((user) => (
+        <div key={user.id} className='card'>
+          <div>
+            <p>Nome: <span>{user.name}</span></p>
+            <p>Endereço: <span>{user.address}</span></p>
+            <p>Email: <span>{user.email}</span></p>
+          </div>
+          <button>
+            <img src={Trash} />
+          </button>
         </div>
-        <button>
-          <img src={Trash}/>
-        </button>    
-      </div>  
+      ))}
+
     </div>
 
   )
