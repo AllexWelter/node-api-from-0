6e29c309e57ea733efde0de6c9e7dec22c130b79
Allex Welter <allexwelter13@gmail.com>
2024-12-08 07:21:34 -0300

    .

--- Front-End(Cadastro-usuarios)/cadastro-usuarios/src/pages/Home/index.jsx
+++ Front-End(Cadastro-usuarios)/cadastro-usuarios/src/pages/Home/index.jsx
@@ -1,12 +1,36 @@
 import './style.css'
+import Trash from '../../assets/trash.png'
 
 function Home() {
 
+    const users = [{
+
+    }, {
+
+    }
+  ]
 
   return (
 
-    <div>
-      <h1>Olá React!</h1>
+    <div className='container'>
+      <form>
+        <h1>Cadastro de Usuários</h1>
+        <input name='nome' type='text'/>
+        <input name='idade' type='number'/>
+        <input name='email' type='email'/>
+        <button type='button'>Cadastrar</button>  
+      </form>
+
+      <div>
+        <div>
+          <p>Nome: </p>
+          <p>Idade: </p>
+          <p>Email: </p>  
+        </div>
+        <button>
+          <img src={Trash}/>
+        </button>    
+      </div>  
     </div>
 
   )
